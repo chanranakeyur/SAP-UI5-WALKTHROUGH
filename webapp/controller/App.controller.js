d87eaa0da147a8d4c2a17dc7003507e29179a2a5
chandaranakeyur <chandaranakeyur@gmail.com>
2023-05-02 21:13:07 +0530

    step 8 completed

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,31 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model/json/JSONModel","sap/ui/model/resource/ResourceModel"
+], function (Controller,
+	MessageToast,
+	JSONModel,
+	ResourceModel) {
     'use strict';
-    return Controller.extend("App.Controller.App", {
-        onInit: function () {},
-        onShowHello: () => {
-            MessageToast.show("Hello There!!!");
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", { /**
+         * @override
+         */
+        onInit: function () {
+            var oData = {
+                recipient: {
+                    name: "World"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+            var i18nModel = new ResourceModel({
+                bundleName:"sap.ui.demo.walkthrough.i18n.i18n"
+            });
+            this.getView().setModel(i18nModel,"i18n")
+        },
+        onShowHello: function () {
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg",[sRecipient]);
+            MessageToast.show(sMsg);
         }
     });
 });
